d5b871d0e89227f11d482d2603961d89c520d186
Christian Sousa <chso8299@colorado.edu>
2021-04-24 21:48:04 -0600

    worked on location code

--- controllers/Follower_Car/Follower_Car.py
+++ controllers/Follower_Car/Follower_Car.py
@@ -1,6 +1,7 @@
 """Follower_Car controller."""
 
 from controller import Robot, Motor, Camera
+from controller import CameraRecognitionObject as CRO
 
 timestep = 10
 
@@ -16,6 +17,33 @@ MOTOR_RIGHT = robot.getDevice('right wheel motor')
 cam = robot.getDevice('good_camera')
 cam.enable(timestep)
 cam.recognitionEnable(timestep)
-#cam.
+#getting the object
+#https://stackoverflow.com/questions/60091830/how-to-recognize-objects-using-camera-on-webots
+
+
+#leader_id= leader.get_id()
+#pos= leader.get_postition()
+flag = True
+size=0
 while robot.step(timestep) != -1:
-   pass
+    if flag:
+        #get size for later
+        l= cam.getRecognitionObjects()[0]
+        size=l.get_size_on_image()
+        print(size)
+        flag= False
+    leader= cam.getRecognitionObjects()[0]
+    #print(leader)
+    leader_id= leader.get_id()
+    #print(leader_id)
+    pos= leader.get_position_on_image()
+    #postition returns a list like this
+    #[num1, num2]
+    #num1 is left or right (lower is left,  higher), camera width is 640 so middle is about 320
+    #num2 is top to bottom which I will ignore because i will be using smaller and larger to see if the 
+    
+    s= leader.get_size_on_image()
+    #print(s)
+    #size one image works the same. gives list [n1,n2] with width height respectivly
+#print(pos)
+    pass
